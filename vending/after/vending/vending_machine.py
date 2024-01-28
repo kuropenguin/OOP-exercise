6814f78c9a538e541c955cf9761916fa73918575
kuropenguin <kuro1994.t@gmail.com>
2024-01-28 14:53:34 +0900

    一旦愚直に step2 elseを使用しないことを実装

--- vending/after/vending/vending_machine.py
+++ vending/after/vending/vending_machine.py
@@ -8,7 +8,7 @@ class VendingMachine:
         self.number_of_100yen = 10
         self.charge = 0
 
-    def buy(self, input_coin, kind_of_drink):
+    def buy(self, input_coin: int, kind_of_drink: int):
         if not self.__can_use_coin(input_coin):
             self.charge += input_coin
             return None
@@ -25,18 +25,21 @@ class VendingMachine:
 
         if input_coin == 100:
             self.number_of_100yen += 1
-        elif input_coin == 500:
+        if input_coin == 500:
             self.charge += (input_coin - 100)
             self.number_of_100yen -= (input_coin - 100) // 100
 
         if kind_of_drink == Drink.COKE:
             self.quantity_of_coke -= 1
-        elif kind_of_drink == Drink.DIET_COKE:
-            self.quantity_of_diet_coke -= 1
-        else:
-            self.quantity_of_tea -= 1
+            return Drink(kind_of_drink)
 
+        if kind_of_drink == Drink.DIET_COKE:
+            self.quantity_of_diet_coke -= 1
+            return Drink(kind_of_drink)
+
+        self.quantity_of_tea -= 1
         return Drink(kind_of_drink)
+
 
     def refund(self):
         result = self.charge
